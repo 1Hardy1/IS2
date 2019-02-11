d983a8591478ff839227c3c29c656bb7b68a4464
Lenovo <Lenovo@Hardy>
2019-02-11 13:52:32 -0500

    a

--- Simulador/src/main/java/uan/edu/co/Simulador/App.java
+++ Simulador/src/main/java/uan/edu/co/Simulador/App.java
@@ -1,13 +1,23 @@
 package uan.edu.co.Simulador;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class App extends Application
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	@Override
+	public void start(Stage stage) throws Exception {	
+		Parent root = FXMLLoader.load(getClass().getResource("/vista/menu.fxml"));
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();   
+	}
+	public static void main( String[] args )
+	{
+	        launch(args);
+	}
 }
