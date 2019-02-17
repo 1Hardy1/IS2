96a0a8116698aa9879a62b764f57b200e0a15cda
JANUS <JANUS@DESKTOP-OTMMTG6>
2019-02-16 23:41:20 -0500

    Clases Junit

---
+++ Simulador/src/main/java/uan/edu/co/Simulador/EquipoAzulTest.java
@@ -0,0 +1,33 @@
+package uan.edu.co.Simulador;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+public class EquipoAzulTest {
+	
+	boolean compruebe = false;
+	int cantidad_del_equipo[] = EquipoAzul.armarEquipoAzul();
+
+	@Test
+	public void armarEquipoTest() {
+		
+		for(int i =0; i<1; i++)
+		{
+			if(cantidad_del_equipo[i] >5 || cantidad_del_equipo[i] < 10)
+			{
+				compruebe = true;
+				System.out.println("bien");
+			}
+			else
+			{
+				fail("Not yet implemented");
+			}
+		}
+		
+		
+	}
+
+}
